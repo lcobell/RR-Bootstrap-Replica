c0d61a97e13a90f37caa6734a8210f15a63c2384
lcobell <lcobell@gmail.com>
2022-05-07 02:58:31 -0500

    I think this meets the requirements

--- src/components/StoreListings.jsx
+++ src/components/StoreListings.jsx
@@ -5,84 +5,26 @@ import Col from "react-bootstrap/Col";
 
 const StoreListings = () => {
   return (
-    <div class="css-5mrw39-Hero">
-      <div class="css-lyxrpb">
-        <h2 class="css-yrjayn">
-          Choose your store in
-          <span tabindex="0" role="button" class="css-1io1d4t">
-            Chicago
-          </span>
-        </h2>
-        <div class="css-121kum4">
-          <ul class="css-mtoayx">
-            <li>
-              <button class="css-1rgc6v9-RetailerCard">
-                <div class="css-1lgfmkt">
-                  <p class="css-3w8d2s">Aldi</p>
-                  <p class="css-vdmd31">
-                    <span>
-                      <span>Delivery</span>
-                      <span class="css-mt4569">&</span>
-                    </span>
-                    <span>
-                      <span>Pickup 2.2mi</span>
-                    </span>
-                  </p>
-                </div>
-                <div class="css-16807jx">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="#c78cd"
-                    xmlns="http://www.w3.org/2000/svg"
-                    size="24"
-                    color="systemGrayscale30"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.879 12l-5.94-5.94a1.5 1.5 0 012.122-2.12l7 7a1.5 1.5 0 010 2.12l-7 7a1.5 1.5 0 01-2.122-2.12L16.88 12z"
-                    ></path>
-                  </svg>
-                </div>
-              </button>
-            </li>
-            <li>
-              <button class="css-1rgc6v9-RetailerCard">
-                <div class="css-1lgfmkt">
-                  <p class="css-3w8d2s">Aldi</p>
-                  <p class="css-vdmd31">
-                    <span>
-                      <span>Delivery</span>
-                      <span class="css-mt4569">&</span>
-                    </span>
-                    <span>
-                      <span>Pickup 2.2mi</span>
-                    </span>
-                  </p>
-                </div>
-                <div class="css-16807jx">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="#c78cd"
-                    xmlns="http://www.w3.org/2000/svg"
-                    size="24"
-                    color="systemGrayscale30"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.879 12l-5.94-5.94a1.5 1.5 0 012.122-2.12l7 7a1.5 1.5 0 010 2.12l-7 7a1.5 1.5 0 01-2.122-2.12L16.88 12z"
-                    ></path>
-                  </svg>
-                </div>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>Marianos</Col>
+        <Col>
+          <img src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/7/cf1e5d97-2c12-4adf-aafd-bdf944c6e654.png" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>Tony's Fresh Market</Col>
+        <Col>
+          <img src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/60/7c3f91e9-a9cf-43cb-86fb-6d503b065af9.png"></img>
+        </Col>
+      </Row>
+      <Row>
+        <Col>Binny's</Col>
+        <Col>
+          <img src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/152/6786ead3-d45c-4614-a32d-35766143ada1.png" />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
